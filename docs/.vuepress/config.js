ef08d207e6c092c49a4d01a82edac683b791f901
guojikun <guojikun@y-open.com>
2020-08-03 11:45:14 +0800

    dddd

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const plugins = require("./config/sidebar.js");
 
 module.exports = {
     title: "fox-design",
-    description: "vue通用组件库",
+    description: "vue通用组件库fox-design",
     themeConfig: {
         nav: navbar,
         sidebar: sidebar,
