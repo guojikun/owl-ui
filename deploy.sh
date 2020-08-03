dc7b3c746224d4ae248ce7b7cab9c0c30d572382
guojikun <guojikun@y-open.com>
2020-08-03 18:56:44 +0800

    优化脚本

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/guojikun/owl-ui.git master:gh-pages
+git push -f git@github.com:guojikun/fox-design.git master:gh-pages
 
 cd -
